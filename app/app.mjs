10dd2015aeadd1e10f678e01cfb5b475faee2e4c
Felipe Chernicharo <string7dev@gmail.com>
2022-03-29 23:44:51 -0400

    FE rooms

--- app/app.mjs
+++ app/app.mjs
@@ -1,6 +1,15 @@
 const socket = io('localhost:3333');
 console.log(socket);
 
+const appState = {
+	user: '',
+	currentRoom: '',
+	rooms: []
+}
+
+const chatRoom = document.querySelector('#chat-room')
+const chatCloseBtn = document.querySelector('#chat-close-btn')
+const chatRoomTitle = document.querySelector('#chat-room-title')
 const roomsList = document.querySelector('#rooms-list');
 const messagesList = document.querySelector('#messages-list');
 const sendMessageForm = document.querySelector('#send-message-form');
@@ -10,6 +19,9 @@ const roomInput = document.querySelector('#room-input');
 
 socket.on('connect', () => {
 	console.log('connected id', socket.id);
+	appState.rooms.push(socket.id);
+	appState.user = socket.id;
+	appendRoom(socket.id);
 });
 
 socket.on('messageReceived', msg => {
@@ -20,23 +32,59 @@ socket.on('messageReceived', msg => {
 sendMessageForm.addEventListener('submit', e => {
 	e.preventDefault();
 	const msg = sendMessageInput.value;
-	const room = roomInput.value;
 
 	if (!msg) return;
 	appendMessage(msg);
 
-	socket.emit('sendMessage', msg, room);
+	socket.emit('sendMessage', msg, appState.currentRoom);
 	sendMessageInput.value = '';
 });
 
 roomForm.addEventListener('submit', e => {
 	e.preventDefault();
 	const room = roomInput.value;
+
 	console.log('join room:', room);
+	socket.emit('joinRoom', room, appendMessage)
+	socket.emit('loadRooms', loadRooms)
 });
+
+chatCloseBtn.addEventListener('click', e => {
+	chatRoom.classList.toggle('closed');
+})
 
 function appendMessage(msg) {
 	const li = document.createElement('li');
 	li.textContent = `${msg}`;
 	messagesList.appendChild(li);
+}
+
+function loadRooms(rooms) {
+	appState.rooms = [...rooms]
+	console.log(appState)
+
+	roomsList.innerHTML = '';
+	rooms.forEach(room => {
+		appendRoom(room)
+	})
+}
+
+function appendRoom(room) {
+	const li = document.createElement('li')
+	li.textContent = `${room}`;
+	li.addEventListener('click', e => {
+		enterRoom(room)
+	})
+	roomsList.appendChild(li)
+}
+
+function enterRoom(room) {
+	if (appState.currentRoom === room) {
+		chatRoom.classList.toggle('closed');
+	} else {
+		chatRoom.classList.remove('closed');
+		appState.currentRoom = room
+		chatRoomTitle.textContent = room;
+	}
+
 }
--- server/io.mjs
+++ server/io.mjs
@@ -35,6 +35,16 @@ io.on('connection', socket => {
 		}
 	});
 
+
+	socket.on('joinRoom', (room) => {
+		socket.join(room)
+	})
+
+	socket.on('loadRooms', (cb) => {
+		cb(Array.from(socket.rooms.values()))
+	})
+
+
 	socket.on('disconnect', () => {
 		console.log(`socket ${socket.id} disconnected`);
 	});
